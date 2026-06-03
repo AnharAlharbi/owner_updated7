2e6d190fc3740df80521b7b2c957f9ff31bd21da
AnharAlharbi <saudanhar99@gmail.com>
2026-06-03 08:46:03 +0300

    owner

--- config_owner.php
+++ config_owner.php
@@ -1,12 +1,17 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "Owner"; 
+// البيانات السحابية الجديدة من موقع Aiven
+$host = "mysql-ed7c027-saudanhar99-3cd7.h.aivencloud.com";
+$user = "avnadmin";
+// ضع الرقم السري الطويل الذي نسخته بعد الضغط على العين 👁️ بين القوسين بالأسفل
+$pass = "AVNS_bYv7LyU4ShxqWBE8YxI"; 
+$db   = "defaultdb";
+$port = 12561; // المنفذ الخاص بالسيرفر السحابي
 
-$conn = new mysqli($host, $user, $pass, $db);
+// إنشاء الاتصال مع تمرير المنفذ (Port)
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
+// التحقق من أن الاتصال يعمل بدون مشاكل
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("فشل الاتصال بقاعدة البيانات السحابية: " . $conn->connect_error);
 }
 ?>
